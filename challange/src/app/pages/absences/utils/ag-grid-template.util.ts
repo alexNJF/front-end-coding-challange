a3acdab2b55eff43e8c145c9ef627a9c68dfd935
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-11-11 11:06:34 +0330

    create global template for Grid

---
+++ challange/src/app/pages/absences/utils/ag-grid-template.util.ts
@@ -0,0 +1,6 @@
+export class AgGridTemplates {
+	static overlayLoadingTemplate: string =
+	'<span style="padding: 10px; border: 2px solid #444; background: #EBECEB;">Loading...</span>';
+	static overlayNoRowsTemplate: string =
+		'<span style="padding: 10px; border: 2px solid #444; background: #EBECEB;">No Data To Show</span>';
+}
