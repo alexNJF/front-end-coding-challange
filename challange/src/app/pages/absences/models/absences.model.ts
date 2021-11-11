831017b5b0ad81583e73823293ff2ce72ca3e328
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-11-11 11:05:04 +0330

    api result model

---
+++ challange/src/app/pages/absences/models/absences.model.ts
@@ -0,0 +1,14 @@
+export interface IAbsences{
+admitterId?:number,
+admitterNote:string,
+confirmedAt:string,
+createdAt:string,
+crewId:number,
+endDate:string,
+id:number,
+memberNote:string,
+rejectedAt:string,
+startDate:string,
+type:string,
+userId:number,
+}
