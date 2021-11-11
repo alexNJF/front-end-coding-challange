ec4d193a30df6e501de96cd13f48541f6c1734db
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-11-11 11:07:38 +0330

    use RXJS (forkjoin ) to handel component logic

--- challange/src/app/pages/absences/components/absences-list/absences-list.component.ts
+++ challange/src/app/pages/absences/components/absences-list/absences-list.component.ts
@@ -1,4 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { forkJoin } from 'rxjs';
+import { AbsencesService, MemberService } from '../../services';
+import { AgGridTemplates, _Array } from '../../utils';
+import { colDef } from './gird.config';
 
 @Component({
   selector: 'app-absences-list',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AbsencesListComponent implements OnInit {
 
-  constructor() { }
+  overlayLoadingTemplate = AgGridTemplates.overlayLoadingTemplate;
+  overlayNoRowsTemplate = AgGridTemplates.overlayNoRowsTemplate;
+  columnDefs = colDef //load config from spratate file
+  rowData;
+  constructor(
+    private absencesService: AbsencesService,
+    private memberService: MemberService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.getData();
   }
 
+  getData(): void {
+    forkJoin([
+      this.absencesService.getAbsences(),
+      this.memberService.getMember()
+    ]).subscribe(
+      (res) => {
+        setTimeout(() => {
+          this.rowData = _Array.mergeBy(res[0].payload, res[1].payload, 'userId')
+        }, 700)
+      },
+      (error) => {
+        this.toastr.warning("Api Call Error")
+        console.log("Api Call Error: ", error);
+      });
+  }
 }
