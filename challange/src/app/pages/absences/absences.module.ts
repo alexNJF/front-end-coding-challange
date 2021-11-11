5d34135f3d07a641f0189a627ae85b0d4ee09155
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-11-11 11:01:28 +0330

    add imports to absent module

--- challange/src/app/pages/absences/absences.module.ts
+++ challange/src/app/pages/absences/absences.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AbsencesRoutingModule } from './absences-routing.module';
 import { AbsencesComponent } from './absences.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { AbsencesListComponent } from './components/absences-list/absences-list.component';
+import { AbsencesService } from './services/absences.service';
+import { MemberService } from './services/member.service';
+import { MomentModule } from 'ngx-moment';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -15,7 +20,13 @@ import { AbsencesListComponent } from './components/absences-list/absences-list.
   imports: [
     CommonModule,
     AgGridModule.withComponents([]),
+    MomentModule,
+    ToastrModule.forRoot(),
+    HttpClientModule,
     AbsencesRoutingModule
+  ],
+  providers:[
+    AbsencesService,MemberService,ToastrService
   ]
 })
 export class AbsencesModule { }
